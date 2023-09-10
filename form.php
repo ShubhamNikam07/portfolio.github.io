0b30c04c067dcf41bbc966cd3a6268a193328972
ShubhamNikam07 <123494710+ShubhamNikam07@users.noreply.github.com>
2023-09-10 11:12:48 +0530

    adding php file

--- form.php
+++ form.php
@@ -2,16 +2,17 @@
 //get data from form  
 $name = $_POST['name'];
 $email= $_POST['email'];
-$subject= $_POST['subject'];
+$subjectIS= $_POST['subject'];
 $message= $_POST['message'];
 $to = "shubhamnikamsn007@gmail.com";
 $subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message. "\r\n subject=" .$subject;
+$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject=" .$subjectIS. "\r\n Message =" . $message;
 $headers = "From: noreply@yoursite.com" . "\r\n" .
 "CC: somebodyelse@example.com";
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
+    header("Location:index.html");
 }
 //redirect
-header("Location:index.html");
+
 ?>
