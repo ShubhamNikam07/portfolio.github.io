1b9c3367b07c888485e23f0d914fdf772d16ab81
ShubhamNikam07 <123494710+ShubhamNikam07@users.noreply.github.com>
2023-09-10 11:33:18 +0530

    Update form.php

--- form.php
+++ form.php
@@ -1,18 +1,35 @@
 <?php
-//get data from form  
-$name = $_POST['name'];
-$email= $_POST['email'];
-$subjectIS= $_POST['subjectIS'];
-$message= $_POST['message'];
-$to = "shubhamnikamsn007@gmail.com";
-$subject = "Mail From website";
-$txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n subject=" .$subjectIS. "\r\n Message =" . $message;
-$headers = "From: noreply@yoursite.com" . "\r\n" .
-"CC: somebodyelse@example.com";
-if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
-    header("Location:index.html");
-}
-//redirect
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Get data from the form
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+    $message = $_POST['message'];
 
+    // Recipient's email address
+    $to = "shubhamnikamsn007@gmail.com";
+
+    // Email subject
+    $email_subject = "Mail From Website";
+
+    // Compose the email message
+    $email_message = "Name: " . $name . "\n";
+    $email_message .= "Email: " . $email . "\n";
+    $email_message .= "Subject: " . $subject . "\n";
+    $email_message .= "Message:\n" . $message;
+
+    // Additional headers
+    $headers = "From: noreply@yoursite.com" . "\r\n";
+    $headers .= "Reply-To: " . $email;
+
+    // Send the email
+    if (mail($to, $email_subject, $email_message, $headers)) {
+        // Email sent successfully, you can redirect the user
+        header("Location: success.html"); // Create this page to display a success message
+        exit();
+    } else {
+        // Email failed to send
+        echo "Error: Email could not be sent.";
+    }
+}
 ?>
